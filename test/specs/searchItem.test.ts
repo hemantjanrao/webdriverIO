76f5fd4adcb1aef3dd0c8d28df402eb5df42461b
Hemant Janrao <hemant.janrao@clark.de>
2021-05-28 18:27:57 +0200

    updated base test

--- test/specs/searchItem.test.ts
+++ test/specs/searchItem.test.ts
@@ -1,7 +1,9 @@
 import HomePage from '../pageobjects/home.page';
+import '../base/base.test';
 
 describe('Search item', () => {
     it('Search a Item on homepage', () => {
+        console.log('Running test');
         HomePage.open();
         HomePage.searchItem('Faded');
     });
